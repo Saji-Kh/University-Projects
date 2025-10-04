1523a0f57358ce961277d032a45d0eb8d219fcf9
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-10-03 19:11:51 -0700

    Add files via upload

---
+++ Temperature/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sajad_Khorsandi_Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double C, F;
+            Console.Write("Enter a temp (C): ");
+            C = Double.Parse(Console.ReadLine());
+
+
+            F = C * 1.8 + 32;
+
+            Console.WriteLine( F );
+            Console.ReadKey();
+        }
+    }
+}
