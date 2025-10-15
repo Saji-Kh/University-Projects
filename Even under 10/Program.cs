9a852aed5475a9b08135e0eb02efefd8d11ba130
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-10-15 13:10:19 -0700

    Add files via upload

---
+++ Even under 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Under_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Enter a number: ");
+            a = int.Parse(Console.ReadLine());
+
+
+            if (a % 2 == 0 & a < 10 )
+            {
+                Console.WriteLine("TRUE!");
+            }
+            else
+            {
+                Console.WriteLine("FALSE!");
+            }
+        }
+    }
+}
