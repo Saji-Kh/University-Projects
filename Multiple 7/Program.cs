f0bf78ae7505c3d35b91b74125c4115407f755f8
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-10-15 13:11:12 -0700

    Add files via upload

---
+++ Multiple 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, a;
+            Console.Write("Enter a number: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 7 == 0)
+            {
+                Console.WriteLine("YES!");
+                }
+            else
+            {
+                c = a * 3;
+                Console.WriteLine(c);
+                Console.ReadKey();
+            }
+        }
+    }
+}
