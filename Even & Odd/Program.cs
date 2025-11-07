b9914436e1fa7dc720ff92fcec3c4609592a07ca
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-11-07 13:24:50 -0800

    Add files via upload

---
+++ Even & Odd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even___Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = new int[10];
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                s[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int num in s)
+            {
+                if (num % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Count of Even numbers: " + evenCount);
+            Console.WriteLine("Count of Odd numbers: " + oddCount);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Even & Odd/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Even & Odd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Even & Odd")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ec43b3e-1cf2-45b8-8347-b9d7438da8d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
