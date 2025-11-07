68b1caf88ef53230694ac158a3c0c5d6933dd5b3
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-11-07 13:23:57 -0800

    Add files via upload

---
+++ Week Days/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_Days
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+            Console.Write("Enter a number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Clear();
+            if (a < 8)
+            {
+                switch (a)
+                {
+                    case 1:
+                        {
+
+                            Console.WriteLine("Saturday");
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            Console.WriteLine("Sunday");
+                            break;
+                        }
+                    case 3:
+                        {
+
+                            Console.WriteLine("Monday");
+                            break;
+                        }
+                    case 4:
+                        {
+
+                            Console.WriteLine("Tuesday");
+                            break;
+                        }
+                    case 5:
+                        {
+
+                            Console.WriteLine("Wednesday");
+                            break;
+                        }
+                    case 6:
+                        {
+
+                            Console.WriteLine("Thursday");
+                            break;
+                        }
+                    case 7:
+                        {
+
+                            Console.WriteLine("Friday");
+                            break;
+
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("END!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
