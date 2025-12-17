91fbc6444e3a9c06ee03540ce3ba8a0c08c229fc
Saji-Kh <sajadkhorsandi061@gmail.com>
2025-12-17 01:27:28 -0800

    Add files via upload

---
+++ Tamrin 5 Soal 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U_Tamrin
+{
+    internal class Program
+    {
+            public static double times(double x, double y)
+        {
+            if (y == 0)
+                return 0;
+            else
+                return x + times(x, y - 1);
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a numbers:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter a numbers:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = times(x, y);
+            Console.Write("{0} * {1} = {2}", x, y, z);
+
+            Console.ReadKey();
+        }
+    }
+}
